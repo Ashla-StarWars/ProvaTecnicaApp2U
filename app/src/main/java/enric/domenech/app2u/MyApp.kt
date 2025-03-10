099eb94ff45389779a69511ec599219a4811440f
Ashla-StarWars <167981174+Ashla-StarWars@users.noreply.github.com>
2025-03-10 21:41:25 +0100

    Second Commit

---
+++ app/src/main/java/enric/domenech/app2u/MyApp.kt
@@ -0,0 +1,20 @@
+package enric.domenech.app2u
+
+import android.app.Application
+import enric.domenech.app2u.di.koin.appModule
+import enric.domenech.app2u.di.koin.initKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.logger.Level
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@MyApp)
+            modules(appModule)
+        }
+    }
+}
---
+++ app/src/main/java/enric/domenech/app2u/ui/navigation/Routes.kt
@@ -0,0 +1,9 @@
+package enric.domenech.app2u.ui.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object HOME
+
+//@Serializable
+//data class DETAIL(val detailId: Int)
